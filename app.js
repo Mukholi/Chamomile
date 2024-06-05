fd01ca8ebed832936e892e35dd23e1e449111167
BillMukholi <work.billmukholi@gmail.com>
2024-06-05 13:45:48 +0300

    Fixed Product Display System

--- app.js
+++ app.js
@@ -46,36 +46,3 @@
 //     console.log(appNavigationData)
 // });
 
-//Product Item Widget
-let productItem = document.getElementsByClassName('product-item')
-
-
-for (let productIndex = 0; productIndex < productItem.length; productIndex++){
-    productItem[productIndex].addEventListener('click',(event)=>{
-        event.preventDefault()
-        var productItemAnimationOpen = gsap.timeline()
-        productItemAnimationOpen.pause()
-
-        var currentProductItemCard = document.getElementsByClassName("app-product-card")[productIndex]
-        var currentProductItemLink = document.getElementsByClassName("app-product-card-link")[productIndex]
-        var currentProductItemCardTop = document.getElementsByClassName("app-product-card-left")[productIndex]
-        var currentProductItemCardTopImageContainer = document.getElementsByClassName("app-product-card-left-image-cont")[productIndex]
-        var currentProductItemCardBottom = document.getElementsByClassName("app-product-card-right")[productIndex]
-        var currentProductItemCardTitle = document.getElementsByClassName("app-product-card-details-title")[productIndex]
-
-
-        productItemAnimationOpen.set(currentProductItemLink,{flexDirection:"column"})
-        // productItemAnimationOpen.set(currentProductItemCardBottom,{alignItems:"flex-start"})
-
-        productItemAnimationOpen.to(currentProductItemCard,{height:"15cm", duration:1})
-        productItemAnimationOpen.to(currentProductItemLink,{height:"15cm", duration:1},"=-1")
-        productItemAnimationOpen.to(currentProductItemCardTop,{width:"100%",height:"10cm", duration:1},"=-0.5")
-        productItemAnimationOpen.to(currentProductItemCardTopImageContainer,{width:"100%",height:"10cm", duration:1},"=-1")
-        
-        productItemAnimationOpen.to(currentProductItemCardTitle,{fontSize:"1.25em", fontWeight:"800"},"=-1")
-
-
-
-        productItemAnimationOpen.restart()
-    })
-}
